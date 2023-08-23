e58aa5f9bb11b8c2e2ce55923b23bd8e7dafa9be
viniciusvk1 <viniciusalmeida.vk1@gmail.com>
2023-08-23 13:03:56 -0300

    Studying Small Automations

---
+++ Small Automation Study/openCalculator.py
@@ -0,0 +1,13 @@
+import pyautogui as mousePosition
+
+mousePosition.sleep(0)
+# print(mousePosition.position())
+
+mousePosition.moveTo(x=13, y=1064)
+mousePosition.click(x=13, y=1064)
+
+mousePosition.sleep(0)
+# print(mousePosition.position())
+
+mousePosition.typewrite("Calculator")
+mousePosition.press("enter")
